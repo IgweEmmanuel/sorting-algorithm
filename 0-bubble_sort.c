fb8d1fc348d17786374ae291b050e5fd31008f0f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-20 16:57:54 +0000

    bubble sort performed with range of array of numbers

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,35 @@
 #include "sort.h"
-
 /**
- * bubble_sort - This sorts array of integers in ascending order
- * @array: This is the array pointer to the array of integers
+ * bubble_sort - This bubble sorts array of numbers
+ * @array: This is the array parameter
  * @size: This is the size of the array
  *
- * Return: This returns the sorted array of integers
+ * Return: It returns sorted array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j;
 	int swapped, temp;
+
 
 	if (size < 2)
 		return;
-
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
 
 		for (j = 0; j < size - i - 1; j++)
-		{
 			if (array[j] > array[j + 1])
 			{
+				/* Swap the elements */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
-				for (k = 0; k < size; k++)
-					printf("%d ", array[k]);
-				printf("\n");
+
+				/* Print swapped arrays*/
+				print_array(array, size);
 			}
-		}
 
 		if (swapped == 0)
 			break;
