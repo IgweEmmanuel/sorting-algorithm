be4f5de645ba3580a4d7663c70f9465a6acc7bb7
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-25 07:57:54 +0000

    count_sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,69 +1,45 @@
 #include "sort.h"
-#include <stdlib.h>
 
 /**
  * counting_sort - Sorts an array of integers in ascending order using
- * counting sort algorithm
  * @array: Array of integers to be sorted
  * @size: Amount of elements in array
  */
 void counting_sort(int *array, size_t size)
 {
 	int i, max;
-	int *count, *result;
+	int *count, *output;
 
 	if (!array || size < 2)
 		return;
-
 	max = array[0];
-	i = 1;
-	while (i < (int)size)
+	for (i = 1; i < (int)size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
-		i++;
 	}
-
 	count = calloc((max + 1), sizeof(int));
 	if (!count)
 		return;
-
-	i = 0;
-	while (i < (int)size)
-	{
+	for (i = 0; i < (int)size; i++)
 		count[array[i]]++;
-		i++;
-	}
 
-	i = 1;
-	while (i <= max)
-	{
+	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
-		i++;
-	}
 	print_array(count, max + 1);
-
-	result = malloc(sizeof(int) * size);
-	if (!result)
+	output = malloc(sizeof(int) * size);
+	if (!output)
 	{
 		free(count);
 		return;
 	}
-
-	i = (int)size - 1;
-	while (i >= 0)
+	for (i = (int)size - 1; i >= 0; i--)
 	{
-		result[count[array[i]] - 1] = array[i];
-		count[array[i]]--; /* Needed to handle identical values */
-		i--;
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
 	}
-
-	i = 0;
-	while (i < (int)size)
-	{
-		array[i] = result[i];
-		i++;
-	}
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
 	free(count);
-	free(result);
+	free(output);
 }
